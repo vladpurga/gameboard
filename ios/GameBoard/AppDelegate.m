0825556be686fcea0dca9a93244bdcd12059dc55
Dave Kerr <dwmkerr@gmail.com>
2017-12-30 20:43:03 +0700

    chore: preupgrade

--- ios/GameBoard/AppDelegate.m
+++ ios/GameBoard/AppDelegate.m
@@ -12,6 +12,8 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+#import <RNGoogleSignin/RNGoogleSignin.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -42,6 +44,13 @@
   rootView.loadingView = launchScreenView;
   
   return YES;
+}
+
+//  React Native Google Signin
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+  
+  return [RNGoogleSignin application:application openURL:url sourceApplication:sourceApplication annotation:annotation];
 }
 
 @end
